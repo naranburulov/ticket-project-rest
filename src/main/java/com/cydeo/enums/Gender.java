dda60b7caf74e5e2786f0d5cc361a24e0ccf9523
naranburulov <naranburulov@gmail.com>
2023-02-01 16:35:09 -0500

    added Gender to enums

---
+++ src/main/java/com/cydeo/enums/Gender.java
@@ -0,0 +1,17 @@
+package com.cydeo.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum Gender {
+
+    MALE("Male"), FEMALE("Female");
+
+    private String value;
+
+    Gender(String value){
+        this.value=value;
+    }
+
+
+}
