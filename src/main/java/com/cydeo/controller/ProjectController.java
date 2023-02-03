a6389702445aa35ff61d95685ea432c48c6fa478
naranburulov <naranburulov@gmail.com>
2023-02-03 11:31:28 -0500

    added ProjectController to controller

--- src/main/java/com/cydeo/controller/ProjectController.java
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -1,19 +1,39 @@
 package com.cydeo.controller;
 
 import com.cydeo.dto.ProjectDTO;
+import com.cydeo.dto.UserDTO;
+import com.cydeo.service.ProjectService;
+import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/project")
 public class ProjectController {
 
+    private final ProjectService projectService;
+    private final UserService userService;
+
+    public ProjectController(ProjectService projectService, UserService userService) {
+        this.projectService = projectService;
+        this.userService = userService;
+    }
+
     @GetMapping("/create")
-    public String createProject(Model model){
+    public String createProject(Model model, @ModelAttribute("username") String username){
         model.addAttribute("project", new ProjectDTO());
-        model.addAttribute("managers", )
+        model.addAttribute("managers", userService.findById(username));
+        model.addAttribute("projects", projectService.findAll());
         return "project/create";
+    }
+
+    @PostMapping("/create")
+    public String insertProject(@ModelAttribute("project") ProjectDTO project){
+        projectService.save(project);
+        return "redirect:/project/create";
     }
 }
