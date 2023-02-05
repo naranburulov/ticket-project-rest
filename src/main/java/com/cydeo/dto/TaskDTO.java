9b7727269ef16245cdc7dba449ce647c6e92b214
naranburulov <naranburulov@gmail.com>
2023-02-05 12:14:32 -0500

    modified TaskDTO with validations

--- src/main/java/com/cydeo/dto/TaskDTO.java
+++ src/main/java/com/cydeo/dto/TaskDTO.java
@@ -1,22 +1,33 @@
 package com.cydeo.dto;
 
 import com.cydeo.enums.Status;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Getter
-@Setter
 public class TaskDTO {
 
     private Long id;
+
+    @NotNull
     private ProjectDTO project;
+
+    @NotNull
     private UserDTO assignedEmployee;
+
+    @NotBlank
     private String taskSubject;
+
+    @NotBlank
     private String taskDetail;
+
     private Status taskStatus;
     private LocalDate assignedDate;
 
