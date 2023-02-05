1068f25360b12c9fccf611846d701d3c81a4abf1
naranburulov <naranburulov@gmail.com>
2023-02-04 19:21:41 -0500

    added editTask() and updateTask() to TaskController

--- src/main/java/com/cydeo/controller/TaskController.java
+++ src/main/java/com/cydeo/controller/TaskController.java
@@ -6,10 +6,7 @@ import com.cydeo.service.TaskService;
 import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/task")
@@ -37,6 +34,21 @@ public class TaskController {
     @PostMapping("/create")
     public String insertTask(TaskDTO task){
         taskService.save(task);
+        return "redirect:/task/create";
+    }
+
+    @GetMapping("/update/{id}")
+    public String editTask(@PathVariable("id") Long taskId, Model model){
+        model.addAttribute("task", taskService.findById(taskId));
+        model.addAttribute("projects", projectService.findAll());
+        model.addAttribute("employees", userService.findEmployees());
+        model.addAttribute("tasks", taskService.findAll());
+        return "/task/update";
+    }
+
+    @PostMapping("/update/{id}")
+    public String updateTask(TaskDTO task){
+        taskService.update(task);
         return "redirect:/task/create";
     }
 
