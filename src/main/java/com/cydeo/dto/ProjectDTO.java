8bd8c8fee5990a8979f866bea8039bc194050dae
naranburulov <naranburulov@gmail.com>
2023-02-05 12:17:07 -0500

    modified ProjectDTO with validations

--- src/main/java/com/cydeo/dto/ProjectDTO.java
+++ src/main/java/com/cydeo/dto/ProjectDTO.java
@@ -4,24 +4,34 @@ import com.cydeo.enums.Status;
 import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Data
-@Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProjectDTO {
 
+
+    @NotBlank
     private String projectName;
+
+    @NotBlank
     private String projectCode;
+
+    @NotNull
     private UserDTO assignedManager;
 
+    @NotNull
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate startDate;
+
+    @NotNull
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate endDate;
 
+    @NotBlank
     private String projectDetail;
     private Status projectStatus;
 
