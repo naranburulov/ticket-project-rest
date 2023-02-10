27333c178d935d6c48d93cf5cbf2937fae02b5b6
naranburulov <naranburulov@gmail.com>
2023-02-10 10:36:12 -0500

    deleted previous service layer, and deleted DataGenerator

--- src/main/java/com/cydeo/controller/ProjectController.java
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -1,75 +1,75 @@
-package com.cydeo.controller;
-
-import com.cydeo.dto.ProjectDTO;
-import com.cydeo.dto.UserDTO;
-import com.cydeo.service.ProjectService;
-import com.cydeo.service.UserService;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-@Controller
-@RequestMapping("/project")
-public class ProjectController {
-
-    private final ProjectService projectService;
-    private final UserService userService;
-
-    public ProjectController(ProjectService projectService, UserService userService) {
-        this.projectService = projectService;
-        this.userService = userService;
-    }
-
-    @GetMapping("/create")
-    public String createProject(Model model, @ModelAttribute("username") String username){
-        model.addAttribute("project", new ProjectDTO());
-        model.addAttribute("managers", userService.findManagers());
-        model.addAttribute("projects", projectService.findAll());
-        return "project/create";
-    }
-
-    @PostMapping("/create")
-    public String insertProject(@ModelAttribute("project") ProjectDTO project){
-        projectService.save(project);
-        return "redirect:/project/create";
-    }
-
-    @GetMapping("/complete/{projectCode}")
-    public String completeProject(@PathVariable("projectCode") String projectCode){
-        projectService.complete(projectService.findById(projectCode));
-        return "redirect:/project/create";
-    }
-
-    @GetMapping("/update/{projectCode}")
-    public String editProject(@PathVariable("projectCode") String projectCode, Model model){
-        model.addAttribute("project", projectService.findById(projectCode));
-        model.addAttribute("managers", userService.findManagers());
-        model.addAttribute("projects", projectService.findAll());
-        return "/project/update";
-    }
-
-    @PostMapping("/update")
-    public String updateProject(@ModelAttribute("project") ProjectDTO project){
-        projectService.update(project);
-        return "redirect:/project/create";
-    }
-
-    @GetMapping("/delete/{projectCode}")
-    public String deleteProject(@PathVariable("projectCode") String projectCode){
-        projectService.deleteById(projectCode);
-        return "redirect:/project/create";
-    }
-
-
-    @GetMapping("/manager/project-status")
-    public String getProjectByManager(Model model){
-        UserDTO manager = userService.findById("john@cydeo.com");   //temporary hard-coded (will change after security)
-        List<ProjectDTO> projects = projectService.getCountedListOfProjects(manager);
-
-        model.addAttribute("projects", projects);
-        return "/manager/project-status";
-    }
-
-}
+//package com.cydeo.controller;
+//
+//import com.cydeo.dto.ProjectDTO;
+//import com.cydeo.dto.UserDTO;
+//import com.cydeo.service.ProjectService;
+//import com.cydeo.service.UserService;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.List;
+//
+//@Controller
+//@RequestMapping("/project")
+//public class ProjectController {
+//
+//    private final ProjectService projectService;
+//    private final UserService userService;
+//
+//    public ProjectController(ProjectService projectService, UserService userService) {
+//        this.projectService = projectService;
+//        this.userService = userService;
+//    }
+//
+//    @GetMapping("/create")
+//    public String createProject(Model model, @ModelAttribute("username") String username){
+//        model.addAttribute("project", new ProjectDTO());
+//        model.addAttribute("managers", userService.findManagers());
+//        model.addAttribute("projects", projectService.findAll());
+//        return "project/create";
+//    }
+//
+//    @PostMapping("/create")
+//    public String insertProject(@ModelAttribute("project") ProjectDTO project){
+//        projectService.save(project);
+//        return "redirect:/project/create";
+//    }
+//
+//    @GetMapping("/complete/{projectCode}")
+//    public String completeProject(@PathVariable("projectCode") String projectCode){
+//        projectService.complete(projectService.findById(projectCode));
+//        return "redirect:/project/create";
+//    }
+//
+//    @GetMapping("/update/{projectCode}")
+//    public String editProject(@PathVariable("projectCode") String projectCode, Model model){
+//        model.addAttribute("project", projectService.findById(projectCode));
+//        model.addAttribute("managers", userService.findManagers());
+//        model.addAttribute("projects", projectService.findAll());
+//        return "/project/update";
+//    }
+//
+//    @PostMapping("/update")
+//    public String updateProject(@ModelAttribute("project") ProjectDTO project){
+//        projectService.update(project);
+//        return "redirect:/project/create";
+//    }
+//
+//    @GetMapping("/delete/{projectCode}")
+//    public String deleteProject(@PathVariable("projectCode") String projectCode){
+//        projectService.deleteById(projectCode);
+//        return "redirect:/project/create";
+//    }
+//
+//
+//    @GetMapping("/manager/project-status")
+//    public String getProjectByManager(Model model){
+//        UserDTO manager = userService.findById("john@cydeo.com");   //temporary hard-coded (will change after security)
+//        List<ProjectDTO> projects = projectService.getCountedListOfProjects(manager);
+//
+//        model.addAttribute("projects", projects);
+//        return "/manager/project-status";
+//    }
+//
+//}
--- src/main/java/com/cydeo/controller/TaskController.java
+++ src/main/java/com/cydeo/controller/TaskController.java
@@ -1,96 +1,96 @@
-package com.cydeo.controller;
-
-import com.cydeo.dto.TaskDTO;
-import com.cydeo.enums.Status;
-import com.cydeo.service.ProjectService;
-import com.cydeo.service.TaskService;
-import com.cydeo.service.UserService;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-
-@Controller
-@RequestMapping("/task")
-public class TaskController {
-
-    private final TaskService taskService;
-    private final ProjectService projectService;
-    private final UserService userService;
-
-    public TaskController(TaskService taskService, ProjectService projectService, UserService userService) {
-        this.taskService = taskService;
-        this.projectService = projectService;
-        this.userService = userService;
-    }
-
-    @GetMapping("/create")
-    public String createTask(Model model){
-        model.addAttribute("task", new TaskDTO());
-        model.addAttribute("projects", projectService.findAll());
-        model.addAttribute("employees", userService.findEmployees());
-        model.addAttribute("tasks", taskService.findAll());
-        return "task/create";
-    }
-
-    @PostMapping("/create")
-    public String insertTask(TaskDTO task){
-        taskService.save(task);
-        return "redirect:/task/create";
-    }
-
-    @GetMapping("/update/{id}")
-    public String editTask(@PathVariable("id") Long taskId, Model model){
-        model.addAttribute("task", taskService.findById(taskId));
-        model.addAttribute("projects", projectService.findAll());
-        model.addAttribute("employees", userService.findEmployees());
-        model.addAttribute("tasks", taskService.findAll());
-        return "/task/update";
-    }
-
-    @PostMapping("/update/{id}")
-    public String updateTask(TaskDTO task){
-        taskService.update(task);
-        return "redirect:/task/create";
-    }
-
-    @GetMapping("delete/{id}")
-    public String deleteTask(@PathVariable("id") Long id){
-        taskService.deleteById(id);
-        return "redirect:/task/create";
-    }
-
-    //Employee part:
-        //pending tasks (status - inProgress tasks):
-    @GetMapping("/employee/pending-tasks")
-    public String employeePendingTasks(Model model){
-        model.addAttribute("tasks", taskService.findAllTasksByStatusIsNot(Status.COMPLETE));
-        return "/task/pending-tasks";
-    }
-
-        //archive tasks (status - completed tasks):
-    @GetMapping("/employee/archive")
-    public String employeeArchiveTasks(Model model){
-        model.addAttribute("tasks", taskService.findAllTasksByStatus(Status.COMPLETE));
-        return "/task/archive";
-    }
-
-    //update Pending Tasks from inProgress to Complete:
-    @GetMapping("/employee/edit/{id}")
-    public String employeeEditTask(@PathVariable("id") Long id, Model model){
-        model.addAttribute("task", taskService.findById(id));
-        model.addAttribute("statuses", Status.values());
-        model.addAttribute("tasks", taskService.findAllTasksByStatusIsNot(Status.COMPLETE));
-        return "/task/status-update";
-    }
-
-    @PostMapping("/employee/update/{id}")
-    public String employeeUpdateTask(TaskDTO task){
-        taskService.updateTaskStatus(task);
-        return "redirect:/task/employee/pending-tasks";
-    }
-
-
-
-
-
-}
+//package com.cydeo.controller;
+//
+//import com.cydeo.dto.TaskDTO;
+//import com.cydeo.enums.Status;
+//import com.cydeo.service.ProjectService;
+//import com.cydeo.service.TaskService;
+//import com.cydeo.service.UserService;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.*;
+//
+//@Controller
+//@RequestMapping("/task")
+//public class TaskController {
+//
+//    private final TaskService taskService;
+//    private final ProjectService projectService;
+//    private final UserService userService;
+//
+//    public TaskController(TaskService taskService, ProjectService projectService, UserService userService) {
+//        this.taskService = taskService;
+//        this.projectService = projectService;
+//        this.userService = userService;
+//    }
+//
+//    @GetMapping("/create")
+//    public String createTask(Model model){
+//        model.addAttribute("task", new TaskDTO());
+//        model.addAttribute("projects", projectService.findAll());
+//        model.addAttribute("employees", userService.findEmployees());
+//        model.addAttribute("tasks", taskService.findAll());
+//        return "task/create";
+//    }
+//
+//    @PostMapping("/create")
+//    public String insertTask(TaskDTO task){
+//        taskService.save(task);
+//        return "redirect:/task/create";
+//    }
+//
+//    @GetMapping("/update/{id}")
+//    public String editTask(@PathVariable("id") Long taskId, Model model){
+//        model.addAttribute("task", taskService.findById(taskId));
+//        model.addAttribute("projects", projectService.findAll());
+//        model.addAttribute("employees", userService.findEmployees());
+//        model.addAttribute("tasks", taskService.findAll());
+//        return "/task/update";
+//    }
+//
+//    @PostMapping("/update/{id}")
+//    public String updateTask(TaskDTO task){
+//        taskService.update(task);
+//        return "redirect:/task/create";
+//    }
+//
+//    @GetMapping("delete/{id}")
+//    public String deleteTask(@PathVariable("id") Long id){
+//        taskService.deleteById(id);
+//        return "redirect:/task/create";
+//    }
+//
+//    //Employee part:
+//        //pending tasks (status - inProgress tasks):
+//    @GetMapping("/employee/pending-tasks")
+//    public String employeePendingTasks(Model model){
+//        model.addAttribute("tasks", taskService.findAllTasksByStatusIsNot(Status.COMPLETE));
+//        return "/task/pending-tasks";
+//    }
+//
+//        //archive tasks (status - completed tasks):
+//    @GetMapping("/employee/archive")
+//    public String employeeArchiveTasks(Model model){
+//        model.addAttribute("tasks", taskService.findAllTasksByStatus(Status.COMPLETE));
+//        return "/task/archive";
+//    }
+//
+//    //update Pending Tasks from inProgress to Complete:
+//    @GetMapping("/employee/edit/{id}")
+//    public String employeeEditTask(@PathVariable("id") Long id, Model model){
+//        model.addAttribute("task", taskService.findById(id));
+//        model.addAttribute("statuses", Status.values());
+//        model.addAttribute("tasks", taskService.findAllTasksByStatusIsNot(Status.COMPLETE));
+//        return "/task/status-update";
+//    }
+//
+//    @PostMapping("/employee/update/{id}")
+//    public String employeeUpdateTask(TaskDTO task){
+//        taskService.updateTaskStatus(task);
+//        return "redirect:/task/employee/pending-tasks";
+//    }
+//
+//
+//
+//
+//
+//}
