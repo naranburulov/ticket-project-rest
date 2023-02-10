42dba685cde11f12f94bd4b1a535cf32a2de9f45
naranburulov <naranburulov@gmail.com>
2023-02-10 11:39:30 -0500

    added RoleMapper to mapper package

---
+++ src/main/java/com/cydeo/mapper/RoleMapper.java
@@ -0,0 +1,22 @@
+package com.cydeo.mapper;
+
+import com.cydeo.dto.RoleDTO;
+import com.cydeo.entity.Role;
+import org.modelmapper.ModelMapper;
+
+public class RoleMapper {
+
+    private final ModelMapper modelMapper;
+
+    public RoleMapper(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
+    public Role convertToEntity(RoleDTO roleDTO){
+        return modelMapper.map(roleDTO, Role.class);
+    }
+
+    public RoleDTO convertToDto(Role role){
+        return modelMapper.map(role, RoleDTO.class);
+    }
+}
