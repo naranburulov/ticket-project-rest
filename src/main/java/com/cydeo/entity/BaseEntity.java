2da97f9fd0e3f9c0504bd4708e8ef60406bb97c6
naranburulov <naranburulov@gmail.com>
2023-02-09 21:56:44 -0500

    modified BaseEntity, Role and User at entity

--- src/main/java/com/cydeo/entity/BaseEntity.java
+++ src/main/java/com/cydeo/entity/BaseEntity.java
@@ -2,12 +2,20 @@ package com.cydeo.entity;
 
 import lombok.*;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@MappedSuperclass
 public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private LocalDateTime insertDateTime;
     private Long insertUserId;
