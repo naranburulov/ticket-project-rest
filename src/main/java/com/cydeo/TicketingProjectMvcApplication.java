f5086ce8cafa210ffb37466be288e762b77ca4f9
naranburulov <naranburulov@gmail.com>
2023-02-10 11:31:30 -0500

    added modelMapper bean to MainRunner class

--- src/main/java/com/cydeo/TicketingProjectMvcApplication.java
+++ src/main/java/com/cydeo/TicketingProjectMvcApplication.java
@@ -1,8 +1,9 @@
 package com.cydeo;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.ui.ModelMap;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TicketingProjectMvcApplication {
@@ -11,6 +12,9 @@ public class TicketingProjectMvcApplication {
         SpringApplication.run(TicketingProjectMvcApplication.class, args);
     }
 
-
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
 
 }
