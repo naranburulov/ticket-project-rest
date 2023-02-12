09667b3b107061c55c82680d7729837ae767fbe7
naranburulov <naranburulov@gmail.com>
2023-02-12 10:42:44 -0500

    added listAllUsers to UserServiceImpl

--- src/main/java/com/cydeo/service/impl/UserServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/UserServiceImpl.java
@@ -1,16 +1,34 @@
 package com.cydeo.service.impl;
 
 import com.cydeo.dto.UserDTO;
+import com.cydeo.mapper.MapperUtil;
+import com.cydeo.repo.RoleRepository;
+import com.cydeo.repo.UserRepository;
 import com.cydeo.service.UserService;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService {
+    private final RoleRepository roleRepository;
+    private final UserRepository userRepository;
+    private final MapperUtil mapperUtil;
+
+    public UserServiceImpl(RoleRepository roleRepository,
+                           UserRepository userRepository, MapperUtil mapperUtil) {
+        this.roleRepository = roleRepository;
+        this.userRepository = userRepository;
+        this.mapperUtil = mapperUtil;
+    }
+
     @Override
     public List<UserDTO> listAllUsers() {
-        return null;
+        return userRepository.findAll(Sort.by("firstName")).stream()
+                .map(user -> mapperUtil.convert(user, new UserDTO()))
+                .collect(Collectors.toList());
     }
 
     @Override
