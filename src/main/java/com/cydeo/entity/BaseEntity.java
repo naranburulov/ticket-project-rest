eadf20221c2871ae65a8143781aa1659d38e4624
naranburulov <naranburulov@gmail.com>
2023-02-13 20:44:04 -0500

    added @Where(clause = "is_deleted=false") to BaseEntity

--- src/main/java/com/cydeo/entity/BaseEntity.java
+++ src/main/java/com/cydeo/entity/BaseEntity.java
@@ -1,6 +1,7 @@
 package com.cydeo.entity;
 
 import lombok.*;
+import org.hibernate.annotations.Where;
 
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 @MappedSuperclass
+@Where(clause = "is_deleted=false")
 public class BaseEntity {
 
     @Id
