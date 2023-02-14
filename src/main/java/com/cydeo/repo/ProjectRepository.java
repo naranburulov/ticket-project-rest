40167892c89c28fd643e0bec792f40e80497bad9
naranburulov <naranburulov@gmail.com>
2023-02-14 13:09:30 -0500

    added ProjectRepository to repo

---
+++ src/main/java/com/cydeo/repo/ProjectRepository.java
@@ -0,0 +1,10 @@
+package com.cydeo.repo;
+
+import com.cydeo.entity.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+    Project findByProjectCode(String projectCode);
+}
