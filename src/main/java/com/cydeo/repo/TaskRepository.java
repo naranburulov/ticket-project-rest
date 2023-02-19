87175d1b402f8949aa546613ef3ae3fdd9f2e183
naranburulov <naranburulov@gmail.com>
2023-02-18 20:14:22 -0500

    modified totalCompletedTasks() in TaskRepository

--- src/main/java/com/cydeo/repo/TaskRepository.java
+++ src/main/java/com/cydeo/repo/TaskRepository.java
@@ -13,13 +13,12 @@ import java.util.List;
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
 
-    //jpql
     @Query("SELECT COUNT(t) FROM Task t WHERE t.project.projectCode = ?1 AND t.taskStatus <> 'COMPLETE'")
     int totalNonCompletedTasks(String projectCode);
 
-    //native
-    @Query(value = "SELECT COUNT(*) FROM tasks t JOIN projects p on t.project_id=p.id +" +
-            "WHERE p.project_code=?1 AND t.task_status='COMPLETE'", nativeQuery = true)
+    @Query(value = "SELECT COUNT(*) " +
+            "FROM tasks t JOIN projects p on t.project_id=p.id " +
+            "WHERE p.project_code=?1 AND t.task_status='COMPLETE'",nativeQuery = true)
     int totalCompletedTasks(String projectCode);
 
     List<Task> findAllByProject(Project project);
