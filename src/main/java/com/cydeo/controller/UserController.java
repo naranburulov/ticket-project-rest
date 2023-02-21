0e15b4a65c8de15c4b6c6a316b240bf70ca3d6d2
naranburulov <naranburulov@gmail.com>
2023-02-21 14:25:38 -0500

    added ResponseWrapper to dto

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -1,70 +1,14 @@
 package com.cydeo.controller;
 
-import com.cydeo.dto.UserDTO;
-import com.cydeo.service.RoleService;
-import com.cydeo.service.UserService;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
+@RestController
+@RequestMapping("/api/v1/user")
+public class UserController{
 
-@Controller
-@RequestMapping("/user")
-public class UserController {
-
-    private final RoleService roleService;
-    private final UserService userService;
-
-    public UserController(RoleService roleService, UserService userService) {
-        this.roleService = roleService;
-        this.userService = userService;
-    }
+    private final
 
 
-    @GetMapping("/create")
-    public String createUser(Model model){
-        model.addAttribute("user", new UserDTO());
-        model.addAttribute("roles", roleService.listAllRoles());
-        model.addAttribute("users", userService.listAllUsers());
-        return "user/create";
-    }
 
-    @PostMapping("/create")
-    public String insertUser(@Valid @ModelAttribute("user") UserDTO user, BindingResult bindingResult, Model model){
-        if (bindingResult.hasErrors()){
-            model.addAttribute("roles", roleService.listAllRoles());
-            model.addAttribute("users", userService.listAllUsers());
-            return "/user/create";
-        }
-        userService.save(user);
-        return "redirect:/user/create";
-    }
-
-    @GetMapping("/update/{username}")
-    public String editUser(@PathVariable String username, Model model){
-        model.addAttribute("user", userService.findByUserName(username));
-        model.addAttribute("roles", roleService.listAllRoles());
-        model.addAttribute("users", userService.listAllUsers());
-        return "user/update";
-    }
-
-    @PostMapping("/update")
-    public String updateUser(@Valid @ModelAttribute("user") UserDTO user, BindingResult bindingResult, Model model){
-
-        if (bindingResult.hasErrors()) {
-            model.addAttribute("roles", roleService.listAllRoles());
-            model.addAttribute("users", userService.listAllUsers());
-            return "/user/update";
-
-        }userService.update(user);
-        return "redirect:/user/create";
-    }
-
-    @GetMapping("/delete/{username}")
-    public String delete(@PathVariable("username") String username){
-        userService.delete(username);
-        return "redirect:/user/create";
-    }
 }
