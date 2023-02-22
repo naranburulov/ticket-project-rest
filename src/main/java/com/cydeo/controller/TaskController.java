debc77c84d2e19fa585e6b846993daa51c5cb02d
naranburulov <naranburulov@gmail.com>
2023-02-21 22:09:28 -0500

    modified endpoint at TaskController

--- src/main/java/com/cydeo/controller/TaskController.java
+++ src/main/java/com/cydeo/controller/TaskController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/task")
+@RequestMapping("/api/v1/task")
 public class TaskController {
 
     private final TaskService taskService;
