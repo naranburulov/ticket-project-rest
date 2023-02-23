0b11c62671a396bef19b7a7206efba7ee5a5fc91
naranburulov <naranburulov@gmail.com>
2023-02-23 14:53:42 -0500

    added SwaggerConfig to config, added few Swagger annotations to UserController

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -3,6 +3,8 @@ package com.cydeo.controller;
 import com.cydeo.dto.ResponseWrapper;
 import com.cydeo.dto.UserDTO;
 import com.cydeo.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/user")
+@Tag(name = "UserController", description = "User API")         //for Swagger
 public class UserController{
 
     private final UserService userService;
@@ -22,6 +25,7 @@ public class UserController{
 
     @GetMapping
     @RolesAllowed({"Manager", "Admin"})
+    @Operation(summary = "Get users")               //for Swagger
     public ResponseEntity<ResponseWrapper> getAllUsers(){
         List<UserDTO> userDTOList = userService.listAllUsers();
         return ResponseEntity.ok(new ResponseWrapper("Users are successfully retrieved", userDTOList, HttpStatus.OK));
@@ -29,6 +33,7 @@ public class UserController{
 
     @GetMapping("/{username}")
     @RolesAllowed("Admin")
+    @Operation(summary = "Get user by username")               //for Swagger
     public ResponseEntity<ResponseWrapper> getUserByName(@PathVariable("username") String userName){
         UserDTO userDTO = userService.findByUserName(userName);
         return ResponseEntity.ok(new ResponseWrapper("User is successfully retried", userDTO, HttpStatus.OK));
@@ -36,6 +41,7 @@ public class UserController{
 
     @PostMapping
     @RolesAllowed("Admin")
+    @Operation(summary = "Create user")               //for Swagger
     public ResponseEntity<ResponseWrapper> createUser(@RequestBody UserDTO userDTO){
         userService.save(userDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseWrapper("User is successfully created", HttpStatus.CREATED));
@@ -43,6 +49,7 @@ public class UserController{
 
     @PutMapping
     @RolesAllowed("Admin")
+    @Operation(summary = "Update user")               //for Swagger
     public ResponseEntity<ResponseWrapper> updateUser(@RequestBody UserDTO userDTO){
         userService.update(userDTO);
         return ResponseEntity.ok(new ResponseWrapper("User is successfully updated",HttpStatus.OK));
@@ -50,6 +57,7 @@ public class UserController{
 
     @DeleteMapping("/{username}")
     @RolesAllowed("Admin")
+    @Operation(summary = "Delete user")               //for Swagger
     public ResponseEntity<ResponseWrapper> deleteUser(@PathVariable("username") String userName){
         userService.delete(userName);
         return ResponseEntity.ok(new ResponseWrapper("User is successfully deleted", HttpStatus.OK));
