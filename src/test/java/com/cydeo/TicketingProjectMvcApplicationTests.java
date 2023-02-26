6d9e0934b0efdffaee20d1339dbd76cc4e82590b
naranburulov <naranburulov@gmail.com>
2023-02-26 10:46:08 -0500

    added password encoder bean to Main Runner class

--- src/test/java/com/cydeo/TicketingProjectMvcApplicationTests.java
+++ src/test/java/com/cydeo/TicketingProjectMvcApplicationTests.java
@@ -1,13 +1,28 @@
 package com.cydeo;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootTest
 class TicketingProjectMvcApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
 }
